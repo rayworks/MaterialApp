1acae9e8f7d93027f8e02b1474d648557da4ff48
rayworks <crayzhou@gmail.com>
2020-08-05 17:48:44 +0800

    Update the view layout

--- app/src/main/java/com/rayworks/materialapp/ScrollingActivity.kt
+++ app/src/main/java/com/rayworks/materialapp/ScrollingActivity.kt
@@ -7,16 +7,45 @@ import android.view.View
 import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
+import kotlin.math.abs
 
 class ScrollingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrolling)
+        setupViewComponents()
+    }
+
+    private fun setupViewComponents() {
         setSupportActionBar(findViewById(R.id.toolbar))
         val toolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)
         toolbarLayout.title = "Rita Wang"//title
+
+        val cardLeft = findViewById<CardView>(R.id.lesson_card_left)
+        val cardRight = findViewById<CardView>(R.id.lesson_card_right)
+
+        val txtProgress = findViewById<TextView>(R.id.text_progress)
+        val txtMore = findViewById<TextView>(R.id.text_more)
+
+        val maxScrollDist = resources.getDimensionPixelSize(R.dimen.vertical_scrollable_distance)
+        val maxTextDist = maxScrollDist / 5
+
+        val appBar = findViewById<AppBarLayout>(R.id.app_bar)
+        appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+            println(">>> verticalOffset : $verticalOffset")
+
+            var toAlpha: Float = 1.0f - abs(verticalOffset) * 1.0f / maxScrollDist
+            cardLeft.alpha = toAlpha
+            cardRight.alpha = toAlpha
+
+            toAlpha = 1.0f - abs(verticalOffset) * 1.0f / maxTextDist
+            txtProgress.alpha = toAlpha
+            txtMore.alpha = toAlpha
+        })
 
         val nameTextView = findViewById<TextView>(R.id.name)
         nameTextView.viewTreeObserver.addOnGlobalLayoutListener(object :
@@ -50,9 +79,6 @@ class ScrollingActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
 
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        return super.onOptionsItemSelected(item)
     }
 }
