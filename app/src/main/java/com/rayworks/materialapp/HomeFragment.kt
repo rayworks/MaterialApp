30040e91002883db1ea425d6a9bf0e74e065dd84
rayworks <crayzhou@gmail.com>
2020-09-04 09:00:56 +0800

    Adjust the layout

--- app/src/main/java/com/rayworks/materialapp/HomeFragment.kt
+++ app/src/main/java/com/rayworks/materialapp/HomeFragment.kt
@@ -1,8 +1,16 @@
 package com.rayworks.materialapp
 
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +23,8 @@ import kotlin.math.abs
 
 class HomeFragment : Fragment() {
     private lateinit var root: View
+
+    private var maxScrollDist = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,8 +43,15 @@ class HomeFragment : Fragment() {
         initView()
     }
 
-    private fun initView() {
+    override fun onHiddenChanged(hidden: Boolean) {
+        super.onHiddenChanged(hidden)
 
+        activity!!.window.statusBarColor =
+            if (hidden) resources.getColor(R.color.colorPrimary)
+            else Color.TRANSPARENT
+    }
+
+    private fun initView() {
         val toolbarLayout: CollapsingToolbarLayout = root.findViewById(R.id.toolbar_layout)
         toolbarLayout.title = "Rita Wang"//title
 
@@ -56,12 +73,12 @@ class HomeFragment : Fragment() {
         val txtProgress = root.findViewById<TextView>(R.id.text_progress)
         val txtMore = root.findViewById<TextView>(R.id.text_more)
 
-        val maxScrollDist = resources.getDimensionPixelSize(R.dimen.vertical_scrollable_distance)
-        val maxTextDist = maxScrollDist / 5
+        maxScrollDist = resources.getDimensionPixelSize(R.dimen.vertical_scrollable_distance)
 
         val appBar = root.findViewById<AppBarLayout>(R.id.app_bar)
         appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
-            println(">>> verticalOffset : $verticalOffset")
+
+            val maxTextDist = maxScrollDist / 5
 
             var toAlpha: Float = 1.0f - abs(verticalOffset) * 1.0f / maxScrollDist
             cardLeft.alpha = toAlpha
@@ -71,6 +88,10 @@ class HomeFragment : Fragment() {
             txtProgress.alpha = toAlpha
             txtMore.alpha = toAlpha
         })
+
+        val bkgView = root.findViewById<View>(R.id.bkg_view)
+        val header = root.findViewById<RelativeLayout>(R.id.profile_header)
+        val hey = root.findViewById<TextView>(R.id.hey)
 
         val nameTextView = root.findViewById<TextView>(R.id.name)
         nameTextView.viewTreeObserver.addOnGlobalLayoutListener(object :
@@ -84,13 +105,33 @@ class HomeFragment : Fragment() {
 
                     println(">> start, top : $start, $top")
 
-                    toolbarLayout.expandedTitleMarginTop = top
+                    toolbarLayout.expandedTitleMarginTop = top - nameTextView.height
                     toolbarLayout.expandedTitleMarginStart = start
+
+                    resolveToolbarLayoutSize()
 
                     nameTextView.visibility = View.INVISIBLE
                 }
             }
         })
+    }
+
+    fun resolveToolbarLayoutSize() {
+        val toolbarLayout: CollapsingToolbarLayout = root.findViewById(R.id.toolbar_layout)
+        toolbarLayout.viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                if (toolbarLayout.height > 0) {
+                    println(">>> toolbarLayout height : ${toolbarLayout.height}")
+
+                    toolbarLayout.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    // for toolbar and cardItem clipped paddings
+                    maxScrollDist = toolbarLayout.height - dpToPx(context!!, 112).toInt()
+                }
+            }
+        })
+
+        toolbarLayout.requestLayout()
     }
 
     private fun getCompatActivity() = (activity as AppCompatActivity)
